5a99d68b05562f5e6f9270754dba19cdbc3615a5
Jackson Chen <90215880+Sma1lboy@users.noreply.github.com>
2024-04-07 14:58:58 -0500

    to: adding more detail need in general information form

--- profile.js
+++ profile.js
@@ -1,7 +1,20 @@
 //finish this later
-let Profile = {
-    Name: "Jackson Chen",
-    ChineseName:"Chong Chen"
+export const Profile = {
+    name: "Jackson Chen",
+    chineseName:"Chong Chen",
+    age: 21,
+    dateOfBirth: "2002-10-01",
+    location: "Madison",
+    description:"I am a Full Stack Developer and also student in University of Wisconsin-Madison major in Computer Science. I'm an explorer of new tech, an avid learner, and a problem-solver at heart. Moreover, the passionate programmer on the earth. Feel free to connect with me for all things tech or just to say hello! Let's shape the future of tech together. 🌟 Since then I pushed 679+ commits, opened 9+ issues, submitted 21+ pull requests, created 0+ gists and contributed to 25+ public repositories",
+    email: "jacksonchen486@gmail.com",
+    languagesSpoken: ["English", "Mandarin"],
+    skills: ["Java", "C#", "C", "JavaScript", "React", "Node.js", "Software Development"],
+    education: {
+        university: "University of Wisconsin-Madison",
+        degree: "Bachelor of Science",
+        major: "Computer Science",
+        graduationDate: "2025",
+        gpa: "3.5"
+    },
 }
 
-export { Profile };
